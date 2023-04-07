49648bce14f61dde9c3901a4ebef3667e5aee6d1
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-04-07 19:13:23 -0300

    chore: add color scheme(nord)

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 require('plugins')
+vim.cmd[[colorscheme nord]]
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,5 @@
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use 'shaunsingh/nord.nvim'
 end)
